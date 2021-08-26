ae05c3ef4818651ef6d41fe1f09bd153514efecd
Rafael Costa <rafaelsevla@gmail.com>
2021-08-26 16:29:48 -0300

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const { src, dest } = require('gulp');
+var concat = require('gulp-concat');
+
+exports.default = function() {
+  return src('dist/*.js').pipe(concat('main.js')).pipe(dest('dist/'));
+}
